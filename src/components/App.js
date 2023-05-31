cb5ca0e1777433a6fa2d5da3a2702ad903d80e8b
JoshuaTownshend <uniformly.anonymous@gmail.com>
2023-05-31 11:19:15 +0900

    router update

--- src/components/App.js
+++ src/components/App.js
@@ -31,7 +31,7 @@ function App() {
 
   return (
     <div className="wrapper">
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <>
           <div className='links'>
             <ul>
