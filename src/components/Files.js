1f9a6beb05731f2330db80ff70b64c5078e90fd8
JoshuaTownshend <uniformly.anonymous@gmail.com>
2024-04-17 11:30:02 +0900

    files passowrd

--- src/components/Files.js
+++ src/components/Files.js
@@ -6,71 +6,97 @@ import cardGenres from './files.json';
 
 //pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.js`;
 
-
 function Files() {
+    const [password, setPassword] = useState('');
+    const [loggedIn, setLoggedIn] = useState(false);
 
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleLogin = () => {
+        if (password === process.env.REACT_APP_PASSWORD) {
+            setLoggedIn(true);
+        } else {
+            alert('Incorrect password!');
+        }
+    };
 
     return (
         <div>
             <h1>Login</h1>
-            You should not be here...
+            {!loggedIn && (
+                <div>
+                    <input
+                        type="password"
+                        placeholder="Enter password"
+                        value={password}
+                        onChange={handlePasswordChange}
+                    />
+                    <button onClick={handleLogin}>Login</button>
+                </div>
+            )}
 
-            <div className="App">
-                <a className="noPurple" href="/pdf/1.pdf" target="_blank">Lets try 1 Writing pdf</a><br></br>
-                <a className="noPurple" href="/pdf/2.pdf" target="_blank">Lets try 2 Writing pdf</a><br></br>
-                <a className="noPurple" href="/pdf/quiz.pdf" target="_blank">3-8 quiz</a>
-            </div>
+            {loggedIn && (
+                <div>
+                    <div className="App">
+                        <a className="noPurple" href="/pdf/1.pdf" target="_blank">Lets try 1 Writing pdf</a><br></br>
+                        <a className="noPurple" href="/pdf/2.pdf" target="_blank">Lets try 2 Writing pdf</a><br></br>
+                        <a className="noPurple" href="/pdf/quiz.pdf" target="_blank">3-8 quiz</a>
+                    </div>
 
-            <table>
-        <thead>
-          <tr>
-            <th>Genre</th>
-            <th>Small Cards</th>
-            <th>Worksheets</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Object.keys(cardGenres).map((genre, index) => {
-            const genreItems = cardGenres[genre];
-            const hasSmallCards = genreItems.some(item => item['small cards']);
-            const hasWorksheets = genreItems.some(item => item.worksheet);
-            
-            if (!hasSmallCards && !hasWorksheets) return null;
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Genre</th>
+                                <th>Small Cards</th>
+                                <th>Worksheets</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {Object.keys(cardGenres).map((genre, index) => {
+                                const genreItems = cardGenres[genre];
+                                const hasSmallCards = genreItems.some(item => item['small cards']);
+                                const hasWorksheets = genreItems.some(item => item.worksheet);
 
-            return (
-              <tr key={index}>
-                <td>{genre}</td>
-                <td>
-                  {hasSmallCards && genreItems.map((item, idx) => (
-                    <div key={idx}>
-                    {item['small cards'] && (
-                      <a className="noPurple" href={item['small cards']} target="_blank" rel="noopener noreferrer">
-                        {item['small cards']}
-                      </a>
-                    )}
-                  </div>
-                  ))}
-                </td>
-                <td>
-                  {hasWorksheets && genreItems.map((item, idx) => (
-                    <div key={idx}>
-                    {item.worksheet && (
-                      <a className="noPurple" href={item.worksheet} target="_blank" rel="noopener noreferrer">
-                        {item.worksheet}
-                      </a>
-                    )}
-                  </div>
-                  ))}
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+                                if (!hasSmallCards && !hasWorksheets) return null;
 
-            {/** <Document file="/pdf/1.pdf">
-                <Page pageNumber={1} />
-    </Document> */}
+                                return (
+                                    <tr key={index}>
+                                        <td>{genre}</td>
+                                        <td>
+                                            {hasSmallCards && genreItems.map((item, idx) => (
+                                                <div key={idx}>
+                                                    {item['small cards'] && (
+                                                        <a className="noPurple" href={item['small cards']} target="_blank" rel="noopener noreferrer">
+                                                            {item['small cards']}
+                                                        </a>
+                                                    )}
+                                                </div>
+                                            ))}
+                                        </td>
+                                        <td>
+                                            {hasWorksheets && genreItems.map((item, idx) => (
+                                                <div key={idx}>
+                                                    {item.worksheet && (
+                                                        <a className="noPurple" href={item.worksheet} target="_blank" rel="noopener noreferrer">
+                                                            {item.worksheet}
+                                                        </a>
+                                                    )}
+                                                </div>
+                                            ))}
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+
+                    {/** <Document file="/pdf/1.pdf">
+                        <Page pageNumber={1} />
+                    </Document> */}
+                </div>
+            )}
         </div>
     );
 }
